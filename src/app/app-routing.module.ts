bf4d8daabea1fb89fe4ac756f0b81becba3e80b2
José Trindade <trindade.jose77@gmail.com>
2020-11-16 12:06:32 +0000

    adding mobile paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,38 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ChatComponent } from './pages/themes/chat/chat.component';
 
-const routes: Routes = [
+const mobilePaths: Routes = [
+  {
+    path: 'mobile',
+    canActivate: [UserLoggedInGuard],
+    component: HomeComponent,
+    children: [
+      {
+        path: 'chat',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
+      },
+      {
+        path: 'user',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)
+      },
+      {
+        path: 'settings',
+        canActivate: [UserLoggedInGuard],
+        component: SettingsComponent
+      },
 
+      {
+        path: 'dashboard',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+    ]
+  },
+];
+
+const routes: Routes = [
   {
     path: 'login',
     canActivate: [UserNotLoggedInGuard],
@@ -18,38 +48,39 @@ const routes: Routes = [
     path: 'chat-t',
     component: ChatComponent
   },
-  {
-    path: 'chat',
-    canActivate: [UserLoggedInGuard],
-    loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
-  },
-  {
-    path: 'user',
-    canActivate: [UserLoggedInGuard],
-    loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)
-  },
-  {
-    path: 'settings',
-    canActivate: [UserLoggedInGuard],
-    component: SettingsComponent
-  },
 
   {
-    path: 'dashboard',
+    path: 'main',
     canActivate: [UserLoggedInGuard],
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-
-
-  {
-    path: '',
-    canActivate: [UserLoggedInGuard],
-    component: HomeComponent,
-    pathMatch: 'full',
     children: [
+      {
+        path: 'chat',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
+      },
+      {
+        path: 'user',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)
+      },
+      {
+        path: 'settings',
+        canActivate: [UserLoggedInGuard],
+        component: SettingsComponent
+      },
 
+      {
+        path: 'dashboard',
+        canActivate: [UserLoggedInGuard],
+        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      ...mobilePaths
     ]
   },
+
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  // { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
